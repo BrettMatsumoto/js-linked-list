279880cc3540ca1b08b9941e18fdf5b08a6a076b
Brett <brett_matsumoto@yahoo.com>
2019-04-05 19:05:03 -1000

    reformatted linkedlist.js

--- linkedList.js
+++ linkedList.js
@@ -10,13 +10,17 @@ function linkedListGenerator() {
   let getHead = function () {
     return head;
   };
+
   let getTail = function () {
     return tail;
   }
+
   let add = function (x) {
-    let newNode = {};
-    newNode.value = x;
-    newNode.next = null;
+    let newNode = {
+      value = x,
+      next = null
+    };
+
     if (head === null) {
       head = newNode;
       tail = newNode;
@@ -24,68 +28,97 @@ function linkedListGenerator() {
       tail.next = newNode;
       tail = newNode;
     }
+
     return newNode;
   }
+
   let remove = function (x) {
     if (x === 0) {
       head = head.next;
       return head;
     }
+
     if (get(x) === false) {
       return false;
     }
+
     if (!get(x + 1)) {
       tail = get(x - 1);
     }
+
+    if (head === null) {
+      return false;
+    }
+
     let previous = get(x - 1);
     let current = get(x);
     previous.next = current.next;
     return previous;
   }
+
   let get = function (x) {
     let current = getHead();
     let count = 0;
+
     if (x === 0) {
+
       return current
     }
+
     if (x < 0) {
+
       return false;
     }
+
     while (count < x) {
+
       current = current.next;
       count++;
+
       if (current === null) {
         return false;
       }
     }
     return current;
   }
+
   let insert = function (x, y) {
-    let newNode = {};
-    newNode.value = x;
-    newNode.next = null;
+    let current = get(y);
+    let previous = get(y - 1)
+
+
+    let newNode = {
+      value = x,
+      next = null
+    };
+
     if (x == undefined) {
       return null;
     }
+
     if (y === 0) { //if looking for head
       newNode.next = head;
       head = newNode;
       return head;
     }
-    if (get(y) === false) {
+
+    if (current === false) {
       return false;
     }
+
     if (!getHead()) {
       head = newNode;
       tail = newNode;
       return head;
     }
-    if (get(y - 1)) {
+
+    if (previous) {
       newNode.next = get(y);
-      get(y - 1).next = newNode;
+      previous.next = newNode;
       return newNode;
     }
   }
+
   let linkedListMethods = {
     getHead: getHead,
     getTail: getTail,
@@ -94,5 +127,6 @@ function linkedListGenerator() {
     get: get,
     insert: insert,
   }
+
   return linkedListMethods
 }
